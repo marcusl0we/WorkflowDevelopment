953803f575c6ed5475960c74cfe32c4360421f4e
Marcus Lowe <marcus.lowe.777@gmail.com>
2023-12-21 19:37:29 +0000

    Ported Day 1 and 2 to c++

--- Port to C++/helloworld.cpp
+++ Port to C++/helloworld.cpp
@@ -1,8 +1,20 @@
 // Your First C++ Program
 
 #include <iostream>
+#include <vector>
+#include <string>
+
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "\nHello World!\n\n";
+
+    std::vector<std::string> msg{"This", "is", "made", "in", "a", "loop\n"};
+
+    for (const std::string &word : msg)
+    {
+        std::cout << word << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
